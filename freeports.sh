9ea2702f1fb9b64b8259a52bf6b50004032d68bb
Denis Gorokhov <gorokhov@gmx.com>
2019-02-12 15:22:00 +0300

    add api user cred

--- freeports.sh
+++ freeports.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
-echo enter username
-read usr
-echo enter password
-read -s pass
+#echo enter username
+#read usr
+#echo enter password
+#read -s pass
 echo enter device Ip Address
 read ip
-curl -s -k -u $usr:$pass "https://172.17.1.205/webacs/api/v4/data/Clients.json?.full=true&deviceIpAddress=$ip" | python -m json.tool | grep -P '(?<=clientInterface":).*$' -o
-ansible-playbook ports.yml -i "$ip,"
+#curl -s -k -u $usr:$pass "https://172.17.1.205/webacs/api/v4/data/Clients.json?.full=true&deviceIpAddress=$ip" | python -m json.tool | grep -P '(?<=clientInterface":).*$' -o
+curl -s -k -u apiuser:Cisco6o66o6 "https://172.17.1.205/webacs/api/v4/data/Clients.json?.full=true&deviceIpAddress=$ip" | python -m json.tool | grep -P '(?<=clientInterface":).*$' -o
+ansible-playbook ports.yml -i "$ip," --vault-password-file=./.vault_pass

921b57f898dd626549b232f14745e4e57d487028
Denis Gorokhov <gorokhov@gmx.com>
2019-02-19 11:20:29 +0300

    add loop

--- reserveport.sh
+++ reserveport.sh
@@ -1,8 +1,6 @@
 echo enter ip
 read s
 ip="${s},"
-echo enter interface
-read int
 echo enter username
 read usr
 echo enter password
@@ -10,32 +8,34 @@ read -s p
 echo enter description
 read DESC
 pass="${p}"
-ansible all -m ios_command -a "username=$usr password=$pass  commands='show interface status | i $int'" -i $ip  -c local
-ansible all -m ios_command -a "username=$usr password=$pass  commands='show running-config interface $int'" -i $ip  -c local
-read -r -p "reserve interface [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-    #add description
-    ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='description $DESC'" -i $ip -c local
+while true
+do
+    echo enter interface
+    read int
     #sho interface
-    ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='shutdown'" -i $ip -c local
     ansible all -m ios_command -a "username=$usr password=$pass  commands='show interface status | i $int'" -i $ip  -c local
     ansible all -m ios_command -a "username=$usr password=$pass  commands='show running-config interface $int'" -i $ip  -c local
-else
-    :
-fi
-read -r -p "reserve more? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-    #add description
-    ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='description $DESC'" -i $ip -c local
-    #sho interface
-    ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='shutdown'" -i $ip -c local
-    ansible all -m ios_command -a "username=$usr password=$pass  commands='show interface status | i $int'" -i $ip  -c local
-    ansible all -m ios_command -a "username=$usr password=$pass  commands='show running-config interface $int'" -i $ip  -c local
-else
-    :
-fi
+    read -r -p "reserve this interface? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        #add description
+        ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='description $DESC'" -i $ip -c local
+        #disable interface
+        ansible all -m ios_config -a "username=$usr password=$pass parents='interface $int' lines='shutdown'" -i $ip -c local
+        #show changes
+        ansible all -m ios_command -a "username=$usr password=$pass  commands='show interface status | i $int'" -i $ip  -c local
+        ansible all -m ios_command -a "username=$usr password=$pass  commands='show running-config interface $int'" -i $ip  -c local
+    else
+        break
+    fi
+    read -r -p "reserve more? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        :
+    else
+        break
+    fi
+done
 read -r -p "save config? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then

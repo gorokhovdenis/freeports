be5e9014b83e7cbb036da5da5168fbb9d36379a1
Denis Gorokhov <gorokhov@gmx.com>
2019-03-21 15:14:54 +0300

    upd

--- code.py
+++ code.py
@@ -1,11 +1,22 @@
 import re
 
+with open ('./free', 'r') as f2:
+    s=f2.readlines()
+    f2=open ('./free','w')
+    for line in s:
+        if re.search(r'Present', line):
+            continue
+        else:
+            f2.write (line)
+    f2.close()
 with open ('./free', 'r') as f:
     text = str(f.read())
-free=(re.findall(r'\b[A-Z]\w\d/\d*', text))
+free=(re.findall(r'\b[A-Z]\w\d/\S*', text))
+#print (free)
 with open ('./reserved', 'r') as f:
     text = str(f.read())
-reserv=(re.findall(r'\d/\d*', text))
+reserv=(re.findall(r'\d/\S*', text))
+#print (reserv)
 result = set(free) - set(reserv) # correct elements, but not yet in sorted order
 #print(sorted(result)) # sort and print
 print ('===========FREE PORTS===========')
